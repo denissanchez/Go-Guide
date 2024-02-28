5aadeff3712f552438ae26e3a0b6594dc6480be9
Denis Sanchez <desanchezca@gmail.com>
2024-02-27 20:51:19 -0500

    Adding tests using mocks

--- testing/main.go
+++ testing/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "time"
+
 func Sum(a, b int) int {
 	return a + b
 }
@@ -16,4 +18,54 @@ func Fibonacci(n int) int {
 		return n
 	}
 	return Fibonacci(n-1) + Fibonacci(n-2)
+}
+
+type Person struct {
+	Code      string
+	Name      string
+	BirthYear int
+}
+
+type Employee struct {
+	Id       string
+	Position string
+}
+
+type FullTimeEmployee struct {
+	Employee
+	Person
+}
+
+var GetPersonByCode = func(code string) (Person, error) {
+	time.Sleep(60 * time.Second)
+	// SELECT * FROM Person WHERE Code = code
+	return Person{Code: "1", Name: "John", BirthYear: 1980}, nil
+}
+
+var GetEmployeeById = func(id string) (Employee, error) {
+	time.Sleep(60 * time.Second)
+	// SELECT * FROM Employee WHERE Id = id
+	return Employee{Id: "1", Position: "Software Developer"}, nil
+}
+
+func GetFullTimeEmpoyeeById(code string, id string) (FullTimeEmployee, error) {
+	var ftEmployee FullTimeEmployee
+
+	e, error := GetEmployeeById(id)
+
+	if error != nil {
+		return ftEmployee, error
+	}
+
+	ftEmployee.Employee = e
+
+	p, error := GetPersonByCode(code)
+
+	if error != nil {
+		return ftEmployee, error
+	}
+
+	ftEmployee.Person = p
+
+	return ftEmployee, nil
 }
--- testing/main_test.go
+++ testing/main_test.go
@@ -54,7 +54,6 @@ func TestFibonacci(t *testing.T) {
 	}{
 		{1, 1},
 		{8, 21},
-		{50, 12586269025},
 	}
 
 	for _, table := range tables {
@@ -62,5 +61,68 @@ func TestFibonacci(t *testing.T) {
 		if total != table.n {
 			t.Errorf("Fibonacci was incorrect, got: %d, want: %d.", total, table.n)
 		}
+	}
+}
+
+func TestGetFullTimeEmployeeById(t *testing.T) {
+	table := []struct {
+		code             string
+		id               string
+		mockFunc         func()
+		expectedEmployee FullTimeEmployee
+	}{
+		{
+			code: "1",
+			id:   "1",
+			mockFunc: func() {
+				GetEmployeeById = func(id string) (Employee, error) {
+					return Employee{Id: "1", Position: "Software Developer"}, nil
+				}
+
+				GetPersonByCode = func(code string) (Person, error) {
+					return Person{Code: "1", Name: "John", BirthYear: 1980}, nil
+				}
+			},
+			expectedEmployee: FullTimeEmployee{
+				Employee: Employee{Id: "1", Position: "Software Developer"},
+				Person:   Person{Code: "1", Name: "John", BirthYear: 1980},
+			},
+		},
+	}
+
+	orginalGetEmployeeById := GetEmployeeById
+	originalGetPersonByCode := GetPersonByCode
+
+	for _, test := range table {
+		test.mockFunc()
+
+		employee, err := GetFullTimeEmpoyeeById(test.code, test.id)
+
+		if err != nil {
+			t.Errorf("GetFullTimeEmployeeById was incorrect, got: %v, want: %v.", err, nil)
+		}
+
+		if employee.Id != test.expectedEmployee.Id {
+			t.Errorf("GetFullTimeEmployeeById was incorrect, got: %v, want: %v.", employee.Id, test.expectedEmployee.Id)
+		}
+
+		if employee.Position != test.expectedEmployee.Position {
+			t.Errorf("GetFullTimeEmployeeById was incorrect, got: %v, want: %v.", employee.Position, test.expectedEmployee.Position)
+		}
+
+		if employee.BirthYear != test.expectedEmployee.BirthYear {
+			t.Errorf("GetFullTimeEmployeeById was incorrect, got: %v, want: %v.", employee.BirthYear, test.expectedEmployee.BirthYear)
+		}
+
+		if employee.Code != test.expectedEmployee.Code {
+			t.Errorf("GetFullTimeEmployeeById was incorrect, got: %v, want: %v.", employee.Code, test.expectedEmployee.Code)
+		}
+
+		if employee.Name != test.expectedEmployee.Name {
+			t.Errorf("GetFullTimeEmployeeById was incorrect, got: %v, want: %v.", employee.Name, test.expectedEmployee.Name)
+		}
+
+		GetEmployeeById = orginalGetEmployeeById
+		GetPersonByCode = originalGetPersonByCode
 	}
 }
